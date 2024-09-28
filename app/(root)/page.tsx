b929a6c60cf2d184e7e8eae763fe15f6963238f5
DharaRioja <cuatita_16@hotmail.com>
2024-09-28 00:40:19 -0400

    add: left sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,34 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Dhara', lastName: 'Rocha'}
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Bobo'}
+            subtext="Access and manage your account and transactions efficently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={2423.67}
+          />
+        </header>
+
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[]}/>
+    </section>
   )
 }
 
