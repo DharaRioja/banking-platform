471bb80c9dfbbc5fc932b6604fb7f649835c9e2b
DharaRioja <cuatita_16@hotmail.com>
2024-09-29 17:57:23 -0400

    add: sign in and up is completed

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -5,9 +5,10 @@ import { Control, FieldPath, Form } from 'react-hook-form'
 import { z } from 'zod'
 import { authFormSchema } from '@/lib/utils'
 
+const formSchema = authFormSchema('sign-up')
 interface CustomInput {
-    control: Control<z.infer<typeof authFormSchema>>,
-    name: FieldPath<z.infer<typeof authFormSchema>>,
+    control: Control<z.infer<typeof formSchema>>,
+    name: FieldPath<z.infer<typeof formSchema>>,
     label: string,
     placeholder: string
 }
@@ -15,7 +16,7 @@ const CustomInput = ({ control, name, label, placeholder }: CustomInput) => {
   return (
     <FormField
     control={control}
-    name="email"
+    name={name}
     render={({ field }) => (
      <div className="form-item">
         <FormLabel className="form-label">
